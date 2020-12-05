545afec3b8b8bbd9a5b406b6274a5fdd272f8a85
Thomas Sojka <thsojka@web.de>
2020-12-05 21:07:29 +0100

    Add crud

--- src/main/tech/thomas_sojka/sevenguis/app.cljs
+++ src/main/tech/thomas_sojka/sevenguis/app.cljs
@@ -1,6 +1,7 @@
 (ns tech.thomas-sojka.sevenguis.app
-  (:require [reagent.dom :as dom]
-            [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [reagent.dom :as dom]
+            [clojure.string :as str]))
 
 (defn task-container [{:keys [title]} children]
   [:div.shadow-lg.mb-6
@@ -167,6 +168,77 @@
         {:type "range" :min 0 :max 30 :value duration :on-change #(swap! duration-state assoc :duration (js/parseInt ^js (.-target.value %)))}]]]
      [button {:class "w-full" :on-click #(swap! duration-state assoc :time 0)} "Reset"]]))
 
+(defonce crud-state
+  (r/atom
+   {:persons [{:name "Hans" :surname "Emil"}
+              {:name "Mustermann" :surname "Max"}
+              {:name "Tisch" :surname "Roman"}]
+    :selected nil
+    :name ""
+    :filter-prefix ""
+    :surname ""}))
+
+(defn drop-index [col idx]
+  (into []
+        (filter identity (map-indexed #(when (not= %1 idx) %2) col))))
+
+(defn crud []
+  (let [{:keys [persons selected name surname filter-prefix]} @crud-state]
+    [:div
+     [:div.mb-4.flex
+      {:style {:width "15rem"}}
+      [:span.mr-3 {:class "w-1/2"} "Filter prefix:"]
+      [input {:class "w-1/2" :value filter-prefix
+              :on-change #(swap! crud-state assoc :filter-prefix %)}]]
+     [:div.flex.mb-4.flex-wrap.md:flex-no-wrap
+      [:ul.overflow-y-auto.h-32.bg-white.border.rounded.mr-3.mb-3.md:mb-0
+       {:style {:width "15rem"}}
+       (->> persons
+            (filter (fn [{:keys [name surname]}]
+                      (or (str/includes?
+                           (str/lower-case name)
+                           (str/lower-case filter-prefix))
+                          (str/includes?
+                           (str/lower-case surname)
+                           (str/lower-case filter-prefix)))))
+            (map-indexed
+             (fn [idx {:keys [name surname]}]
+               [:li {:key idx}
+                [:button.pl-2.py-1.w-full.text-left
+                 {:class (when (= idx selected) "bg-blue-200")
+                  :on-click #(swap! crud-state assoc :selected idx)}
+                 (str surname ", " name)]])))]
+      [:div
+       {:class "w-1/2"}
+       [:div.flex.mb-3
+        [:label {:class "w-1/2" :for "crud-name"} "Name:"]
+        [input {:class "w-32" :id "crud-name"
+                :value name
+                :on-change #(swap! crud-state assoc :name %)}]]
+       [:div.flex
+        [:label {:class "w-1/2" :for "crud-surname"} "Surname:"]
+        [input {:class "w-32" :id "crud-surname"
+                :value surname
+                :on-change #(swap! crud-state assoc :surname %)}]]]]
+     [:div
+      [button
+       {:class "mr-3"
+        :on-click #(when (and (not-empty name) (not-empty surname))
+                     (swap! crud-state update :persons conj
+                            {:name name :surname surname}))}
+       "Create"]
+      [button
+       {:class "mr-3"
+        :on-click #(when (and selected (not-empty name) (not-empty surname))
+                     (swap! crud-state assoc-in [:persons selected]
+                            {:name name :surname surname}))}
+       "Update"]
+      [button
+       {:class "mr-3"
+        :on-click #(when selected
+                     (swap! crud-state update :persons drop-index selected))}
+       "Delete"]]]))
+
 (defn app []
   [:div.p-6.container.mx-auto.text-gray-900
    [:h1.text-2xl.font-semibold.mb-8
@@ -175,7 +247,8 @@
     [task-container {:title "Counter"} [counter]]
     [task-container {:title "Temperature Converter"} [temperature-converter]]
     [task-container {:title "Flight Booker"} [flight-booker]]
-    [task-container {:title "Timer"} [timer]]]])
+    [task-container {:title "Timer"} [timer]]
+    [task-container {:title "CRUD"} [crud]]]])
 
 (dom/render
  [app]
