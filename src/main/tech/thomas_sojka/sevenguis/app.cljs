4981abf958c300d01ba9a816f0cd4c79e2d10593
Thomas Sojka <thsojka@web.de>
2020-12-13 10:47:48 +0100

    Add link to code for all GUIs

--- src/main/tech/thomas_sojka/sevenguis/app.cljs
+++ src/main/tech/thomas_sojka/sevenguis/app.cljs
@@ -10,10 +10,10 @@
              [temperature-converter]]
             [tech.thomas-sojka.sevenguis.timer :refer [timer]]))
 
-(defn task-container [{:keys [title]} children]
+(defn task-container [{:keys [title link]} children]
   [:div.shadow-lg.mb-6
    [:header.bg-gray-200.text-center.rounded-t.border-t.border-l.border-r.border-gray-400
-    title]
+    [:a.underline {:href link} title]]
    [:main.bg-gray-100.p-2.rounded-b.border.border-gray-400
     children]])
 
@@ -23,13 +23,34 @@
    [:h1.text-2xl.font-semibold.mb-8
     [:a {:href "https://eugenkiss.github.io/7guis/tasks"} "7GUIs in Clojure/Reagent"]]
    [:div.flex.flex-col.items-start
-    [task-container {:title "Counter"} [counter]]
-    [task-container {:title "Temperature Converter"} [temperature-converter]]
-    [task-container {:title "Flight Booker"} [flight-booker]]
-    [task-container {:title "Timer"} [timer]]
-    [task-container {:title "CRUD"} [crud]]
-    [task-container {:title "Circle Drawer"} [circle-drawer]]
-    [task-container {:title "Cells"} [cells]]]])
+    [task-container
+     {:title "Counter"
+      :link "https://github.com/rollacaster/7guis-clojure-reagent/blob/main/src/main/tech/thomas_sojka/sevenguis/counter.cljs"}
+     [counter]]
+    [task-container
+     {:title "Temperature Converter"
+      :link "https://github.com/rollacaster/7guis-clojure-reagent/blob/main/src/main/tech/thomas_sojka/sevenguis/temperature_converter.cljs"}
+     [temperature-converter]]
+    [task-container
+     {:title "Flight Booker"
+      :link "https://github.com/rollacaster/7guis-clojure-reagent/blob/main/src/main/tech/thomas_sojka/sevenguis/flight-booker.cljs"}
+     [flight-booker]]
+    [task-container
+     {:title "Timer"
+      :link "https://github.com/rollacaster/7guis-clojure-reagent/blob/main/src/main/tech/thomas_sojka/sevenguis/timer.cljs"}
+     [timer]]
+    [task-container
+     {:title "CRUD"
+      :link "https://github.com/rollacaster/7guis-clojure-reagent/blob/main/src/main/tech/thomas_sojka/sevenguis/crud.cljs"}
+     [crud]]
+    [task-container
+     {:title "Circle Drawer"
+      :link "https://github.com/rollacaster/7guis-clojure-reagent/blob/main/src/main/tech/thomas_sojka/sevenguis/circle_drawer.cljs"}
+     [circle-drawer]]
+    [task-container
+     {:title "Cells"
+      :link "https://github.com/rollacaster/7guis-clojure-reagent/blob/main/src/main/tech/thomas_sojka/sevenguis/cells.cljs"}
+     [cells]]]])
 
 (dom/render
  [app]
